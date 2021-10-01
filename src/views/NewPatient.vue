0506e826e5ea7d215527aa98d768e187e4010113
Letícia Fernandes <leticia.fernandes@tmax.com.br>
2021-09-30 22:18:25 -0300

    createPatient

--- src/views/NewPatient.vue
+++ src/views/NewPatient.vue
@@ -3,20 +3,35 @@
     <Nav />
     <div class="container-fluid">
       <div class="my-3">
-        <b-form>
+        <b-form @submit="createPatient">
           <b-form-group
             label="Nome Completo"
             label-for="nomeCompleto"
             class="mb-3"
           >
-            <b-input id="nomeCompleto" type="text" required></b-input>
+            <b-input
+              id="nomeCompleto"
+              type="text"
+              required
+              v-model="patient.nomeCompleto"
+            ></b-input>
           </b-form-group>
           <div class="row">
             <b-form-group label="RG" label-for="rg" class="mb-3 col-6">
-              <b-input id="rg" type="text" required></b-input>
+              <b-input
+                id="rg"
+                type="text"
+                required
+                v-model="patient.rg"
+              ></b-input>
             </b-form-group>
             <b-form-group label="CPF" label-for="cpf" class="mb-3 col-6">
-              <b-input id="cpf" type="text" required></b-input>
+              <b-input
+                id="cpf"
+                type="text"
+                required
+                v-model="patient.cpf"
+              ></b-input>
             </b-form-group>
           </div>
           <div class="row">
@@ -25,10 +40,20 @@
               label-for="telefone"
               class="mb-3 col-6"
             >
-              <b-input id="telefone" type="text" required></b-input>
+              <b-input
+                id="telefone"
+                type="text"
+                required
+                v-model="patient.telefone"
+              ></b-input>
             </b-form-group>
             <b-form-group label="E-mail" label-for="email" class="mb-3 col-6">
-              <b-input id="email" type="email" required></b-input>
+              <b-input
+                id="email"
+                type="email"
+                required
+                v-model="patient.email"
+              ></b-input>
             </b-form-group>
           </div>
           <b-form-group
@@ -36,7 +61,12 @@
             label-for="prontuarioMedico"
             class="mb-3"
           >
-            <b-input id="prontuarioMedico" type="text" required></b-input>
+            <b-input
+              id="prontuarioMedico"
+              type="text"
+              required
+              v-model="patient.prontuarioMedico"
+            ></b-input>
           </b-form-group>
           <div class="d-flex justify-content-end">
             <b-button variant="secondary" class="mx-3" to="/pacientes">
@@ -59,6 +89,34 @@ export default {
   name: "Novo Paciente",
   components: {
     Nav,
+  },
+  data() {
+    return {
+      doctor: {
+        nomeCompleto: "",
+        rg: "",
+        cpf: "",
+        telefone: "",
+        email: "",
+        prontuarioMedico: "",
+      },
+    };
+  },
+  methods: {
+    createPatient(event) {
+      event.preventDefault();
+      this.$http
+        .post("/api/users", this.patient) 
+        .then((response) => {
+          console.log(response.data);
+          console.log(response);
+          this.$router.push("/home");
+        })
+        .catch((error) => {
+          console.error("Não foi possível realizar o Login");
+          console.error(error);
+        });
+    },
   },
 };
 </script>
